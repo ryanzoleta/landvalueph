8fbfba34e8e003b5151a96033f53d408eafcb616
Ryan Arnold Zoleta <ryanarnold.zoleta@gmail.com>
2023-07-19 13:35:11 +0800

    add metadata

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,16 @@
   import '../app.css';
 </script>
 
+<svelte:head>
+  <title>Land Value PH</title>
+  <meta
+    name="description"
+    content="landvalue.ph : interactive map of property values in Greater Metro Manila area Philippines" />
+  <link
+    rel="icon"
+    href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🗺️</text></svg>" />
+</svelte:head>
+
 <main class="h-screen min-h-screen">
   <slot />
 </main>
